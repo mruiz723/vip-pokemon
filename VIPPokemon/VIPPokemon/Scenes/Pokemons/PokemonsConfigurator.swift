5a7695a70674b76b2032f4e20d2594b0262b8ca4
Marlon Ruiz <mruiz723@gmail.com>
2021-11-10 15:10:06 -0500

    Adding configurator.

---
+++ VIPPokemon/VIPPokemon/Scenes/Pokemons/PokemonsConfigurator.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonsConfigurator.swift
+//  VIPPokemon
+//
+//  Created by Marlon David Ruiz Arroyave on 10/11/21.
+//
+
+import UIKit
+
+struct PokemonsConfigurator {
+
+    static func configureModule(viewController: PokemonsViewController) {
+        let interactor = PokemonsInteractor()
+        let presenter = PokemonsPresenter()
+        let router = PokemonsRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+}
